93eff77b96013e3ac4f8f4f340983658d9d05c58
Justin McCullough <justin@cretin.me>
2018-01-24 11:51:26 -0800

    Added support for removing components from the work table. Added a grid for evenly spacing tiles on the work table.

--- www/assets/js/index.js
+++ www/assets/js/index.js
@@ -1,11 +1,22 @@
+
+var selectingComponents = false;
+var selected = [];
+
 $( function() {
 
-	 $(".drag-to-canvas")
+	var holder = $("<div></div>");
+	for (var i = 0; i < 100; i++) {
+		holder.append($("<div></div>").addClass("tile-slot"));
+	}
+	$(".work-table").append(holder.html());
+
+	$(".drag-to-canvas")
 	 	.draggable({
-	 		//grid: [120, 120],
 	 		containment: ".work-table",
-	 		zIndex: $(this).attr("rel") == "harness" ? 2 : 1,
-	 		snap: $(this).attr("rel") == "harness" ? ".zone" : null,
+	 		zIndex: $(this).attr("rel") == "harness" ? 2 : 0,
+	 		snap: $(this).attr("rel") == "harness" ? ".zone" : ".tile-slot",
+	 		snapMode: "inner",
+	 		snapTolerance: 50,
 	 		helper: function() {
 	 			var component = "";
 
@@ -14,57 +25,47 @@ $( function() {
 	 				case "harness"	: component = "./assets/img/tile/harness_180x22.png"; break;
 	 			}
 
-	 			return $("<img />")
+	 			var helper = $("<img />")
 	 				.attr({ src: component, rel: $(this).attr("rel") });
+
+	 			console.log("using helper", helper);
+	 			return helper;
 	 		},
-	 		stop: function(e, ui) {
-	 			var board = $(".work-table");
-
-	 			if (ui.helper.attr("rel") == "tile") {
-	 				$('	\
-		 				<div class="tile"> \
-							<div class="zone top left"></div> \
-							<div class="zone top right"></div> \
-							<div class="zone bottom left"></div> \
-							<div class="zone bottom right"></div> \
-						</div> \
-					')
-					.attr("src", ui.helper.attr("src"))
-					.addClass("drag-to-canvas component")
-					.css({
-						position: "absolute",
-						left: ui.offset.left - board.offset().left,
-						top: ui.offset.top - board.offset().top,
-						zIndex: 2,
-					})
-					.draggable({
-						grid: [135, 135],
-	 					containment: ".work-table",
-					})
-					.appendTo($(".work-table"));
-	 			} else if (ui.helper.attr("rel") == "harness") {
-	 				$("<img />")
-						.attr("src", ui.helper.attr("src"))
-						.addClass("drag-to-canvas component")
-						.css({
-							position: "absolute",
-							left: ui.offset.left - board.offset().left,
-							top: ui.offset.top - board.offset().top,
-							zIndex: 2,
-						})
-						.draggable({
-							//grid: [20, 20],
-		 					containment: ".work-table",
-						})
-						.appendTo($(".work-table"));
-	 			}
-
-	 		}
 	 	});
 
-	 $(".work-table")
+	 $(".tile-slot")
 	 	.droppable({
-	 		accept: ".drag-to-canvas"
+	 		accept: "[rel=tile]",
+	 		drop: function(e, ui) {
+	 			var droppable = $(this);
+
+ 				var tile = $('	\
+	 				<div class="tile"> \
+						<div class="zone left"></div> \
+						<div class="zone right"></div> \
+						<div class="zone top"></div> \
+						<div class="zone bottom"></div> \
+					</div> \
+				')
+				.attr("src", ui.helper.attr("src"))
+				.click(function() {
+					var i = -1;
+
+					if (selectingComponents) {
+						if ((i = selected.indexOf(this)) >= 0) {
+							$(this).children(".highlight").remove();
+							selected.splice(i, 1);
+						} else {
+							$(this).append($("<div class='highlight'></div>"));
+							selected.push(this);
+						}
+					}
+				})
+				.appendTo(droppable);
+
+				droppable.droppable("option", "disabled", true);
+				configureTileDrops();
+	 		}
 	 	});
 
 	$("#footerHandle").on("click", function() {
@@ -81,4 +82,55 @@ $( function() {
 			handle.switchClass("ion-arrow-down-a", "ion-arrow-up-a");
 		}
 	});
+
+
 });
+
+function selectComponent(ev) {
+	selectingComponents = !selectingComponents;
+	console.log("select mode", selectingComponents)
+	if (selectingComponents) {
+		$(".work-table").css({cursor: "pointer"});
+	} else {
+		$(".work-table").css({cursor: "default"});
+	}
+}
+
+function removeComponents() {
+	while (selected.length > 0) {
+		var component = $(selected.pop());
+
+		component.parent().droppable("option", "disabled", false);
+		component.remove();
+	}
+
+	selectComponent();
+}
+
+function configureTileDrops() {
+	$(".tile > .zone")
+ 	.droppable({
+ 		accept: "[rel=harness]",
+ 		drop: function(e, ui) {
+ 			var droppable = $(this);
+
+			$("<img />")
+				.attr({ src: ui.helper.attr("src") })
+				.css({ zIndex: 2 })
+				.appendTo(droppable);
+
+			droppable
+				.droppable("option", "disabled", true)
+				.css({zIndex: 2});
+
+			configureTileDrops();
+ 		},
+ 		over: function(e, ui) {
+ 			$(this).css({border: "3px solid blue"});
+ 		},
+ 		out: function(e, ui) {
+ 			$(this).css({border: "1px solid red"});
+ 		}
+ 	});
+
+}
